f6fcba09047abff10537c6d845d690b32b10b40d
Maruboina Jyothi Venkata Sai Ratna Kumar <54733872+kum3445@users.noreply.github.com>
2022-07-21 21:53:59 +0530

    Update index.js
    Disabled right click on images using JavaScript.

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var all=document.querySelectorAll("#photos img");
-console.log(all);
 for(var i=0;i<all.length;i++){
     all[i].setAttribute("onclick","enlarge(this,"+Math.floor((i)/3+1)+")");
 }
 
 var allrow=document.querySelectorAll("#photos .row");
-console.log(allrow);
 for(var i=0;i<allrow.length;i++){
     var j=i+1;
-    allrow[i].classList.add("row"+j)
+    allrow[i].classList.add("row"+j);
 }
+
+var allimg=document.querySelectorAll("img")
+for(var i=0;i<allimg.length;i++){
+    allimg[i].setAttribute("oncontextmenu","return false");
+}
+
 function enlarge(image,n){
     var bigimage= document.querySelector("#photos .container-fluid .row"+n.toString());
     var prev=bigimage.innerHTML;
-    bigimage.innerHTML="<img src='"+image.src+"'>";
-    bigimage.querySelector("img").addEventListener("click", function(){ bigimage.innerHTML=prev})
+    bigimage.innerHTML="<img src="+image.src+">";
+    bigimage.setAttribute("oncontextmenu","return false");
+    bigimage.querySelector("img").addEventListener("click", function(){ bigimage.innerHTML=prev});
 
 }
